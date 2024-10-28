42acb0dc63c810dc11553f785da495000ddcc7df
Ambrose Bonnaire-Sergeant <ambrose@ambrosebs.com>
2024-10-27 21:13:10 -0500

    wip

--- test/overtone/sc/synth_test.clj
+++ test/overtone/sc/synth_test.clj
@@ -1,8 +1,13 @@
 (ns overtone.sc.synth-test
   (:require [clojure.test :refer [deftest is]]
             [overtone.sc.synth :as sut]
-            [overtone.sc.ugens :as ug]))
+            [overtone.sc.ugens :as ug]
+            [overtone.sc.ugen-collide :as u]
+            [overtone.sc.envelope :as env]
+            [clojure.data :as data]))
 
 (deftest topological-sort-ugens-test
-  (is (= ::FIXME (sut/topological-sort-ugens (ug/sin-osc 440))))
+  (let [u (ug/sin-osc 440)]
+    (is (= [u] (sut/topological-sort-ugens #{u}))))
+  ;(sut/topological-sort-ugens #{(ug/out 0 (u/* [1 2] [3 4]))})
   )
